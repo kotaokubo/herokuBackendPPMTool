30616878d66aa72770d0bd27c8fa651507d71dbe
kotaokubo <kutpremium@gmail.com>
2020-02-23 21:46:53 +0900

    react app

---
+++ src/main/resources/static/precache-manifest.8d973df90adf657c2ad761da8d305a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6816b624f583bf5264f06a627e6b5134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3fd227c1cacec75b6d3",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e809bd38017f6e808f5d",
+    "url": "/static/css/main.682e5fd6.chunk.css"
+  },
+  {
+    "revision": "f3fd227c1cacec75b6d3",
+    "url": "/static/js/2.d3e59894.chunk.js"
+  },
+  {
+    "revision": "e809bd38017f6e808f5d",
+    "url": "/static/js/main.3eb9d840.chunk.js"
+  },
+  {
+    "revision": "bff4cfe4cf29287808c4",
+    "url": "/static/js/runtime-main.0a7c8d69.js"
+  }
+]);
